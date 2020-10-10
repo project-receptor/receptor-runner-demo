5a479ed5fbf94d5eb05850c50472eb8bd3198cc0
Graham Mainwaring <graham@mhn.org>
2020-10-10 19:57:32 -0400

    Install Runner and Ansible in image layers instead of commands

--- test-startup.sh
+++ test-startup.sh
@@ -3,38 +3,52 @@ set -e
 
 . ./test-env.sh
 
+# List of nodes
+NODES="node1 node2 node3"
+
+# Get the base Receptor image
+$CMD pull quay.io/project-receptor/receptor:latest
+$CMD tag quay.io/project-receptor/receptor:latest receptor:latest
+
+# Build the individual node images
+for node in $NODES; do
+  $CMD build containers/$node --tag receptor-$node:latest
+done
+
 # Create custom network
 $CMD network create -d bridge \
   --subnet 172.30.99.0/24 \
   --gateway 172.30.99.1 \
   receptor
 
-# Get the image
-$CMD pull quay.io/project-receptor/receptor:latest
-$CMD tag quay.io/project-receptor/receptor:latest receptor:latest
-
-# Run Receptor nodes
-for node in node1 node2 node3; do
+# Run the Receptor nodes
+for node in $NODES; do
   $CMD run -d --rm \
     --name $node \
     --hostname "$node.receptor" \
     --network receptor \
-    --volume $PWD/$node:/etc/receptor:Z \
-    receptor:latest
+    --volume $PWD/conf/$node:/etc/receptor:Z \
+    receptor-$node:latest
 done
-
-# Install Ansible Runner in node1 and node3
-for node in node1 node3; do
-  $CMD exec $node bash -c 'dnf -y install git gcc python-devel &&
-    pip install git+https://github.com/ansible/ansible-runner' &
-done
-wait
 
 # Copy demo project to node1
 $CMD cp $PWD/demo node1:/demo
 
-# Install Ansible itself in node3
-$CMD exec node3 bash -c 'pip install ansible'
+# Wait for nodes to be known
+ready=0
+while [ "$ready" -eq 0 ]; do
+  status=$($CMD exec node1 receptorctl status)
+  echo "$status"
+  ready=1
+  for node in $NODES; do
+    if ! echo "$status" | grep $node > /dev/null; then
+      echo "Network not ready: $node not known"
+      ready=0
+    fi
+  done
+  if [ "$ready" -eq 0 ]; then
+    sleep 1
+    echo
+  fi
+done
 
-# Show Receptor status
-$CMD exec node1 receptorctl status
